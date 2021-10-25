61ef7209135a1ebdaadfc3064f661fc0ed5cf02d
PatrykMrozinski <patryk.mrozinski@trustflight.io>
2021-10-25 20:44:59 +0100

    fix style import path

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Navbar from "../Navbar"
 
-import "../styles/global.css"
+import "../../styles/global.css"
 
 export const Layout = ({ children }) => {
   return (
